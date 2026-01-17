9c9495dd5685a05bb1958d85c0648aa4d616bd77
koyahness <cryptomakoya@gmail.com>
2026-01-17 13:47:32 +0200

    updates

--- in-tutorial/mapping.sol
+++ in-tutorial/mapping.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 
 contract Mappings {
 
-    mapping (address => uint) favoriteNumbers;
+    mapping (address => uint) public favoriteNumbers;
 
     function saveFavoriteNumber(address _address, uint _favorite) public {
-    favoriteNumbers[_address] = _favorite;
+    favoriteNumbers[_address] = _favorite; //save to mapping with address as the key
 }
 
 }
