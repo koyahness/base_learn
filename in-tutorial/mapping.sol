2cd9a3ae5391f8fd8b1bdf88b5514c301c0c4e7b
koyahness <cryptomakoya@gmail.com>
2026-01-17 13:44:37 +0200

    updated

---
+++ in-tutorial/mapping.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Mappings {
+
+    mapping (address => uint) favoriteNumbers;
+
+    function saveFavoriteNumber(address _address, uint _favorite) public {
+    favoriteNumbers[_address] = _favorite;
+}
+}
