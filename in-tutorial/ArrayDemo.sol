21844e5fda517eeccad06ae038b5c4bdc9bb941c
koyahness <cryptomakoya@gmail.com>
2026-01-17 10:35:38 +0200

    updates

--- in-tutorial/ArrayDemo.sol
+++ in-tutorial/ArrayDemo.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
-
 contract ArrayDemo {
     uint[] public numbers = [1,2,3,4,5,6,7,8,9,10];
 
     function _countEvenNumbers() internal view returns(uint) {
-        
+
     uint result = 0;
 
     for(uint i = 0; i < numbers.length; i++) {
