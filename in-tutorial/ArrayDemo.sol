f48b4b0788ab79d08c04842dc0ce27bc66d52bf0
koyahness <cryptomakoya@gmail.com>
2026-01-17 10:22:12 +0200

    add base learn in tutorial contracts

---
+++ in-tutorial/ArrayDemo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract ArrayDemo {
+    uint[] public numbers = [1,2,3,4,5,6,7,8,9,10];
+
+    function getEvenNumbers() external view returns(uint[] memory) {
+        // TODO
+    }
+}
