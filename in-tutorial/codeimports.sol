a0426d814ac6d1fd339522883cb19f85458b2d9f
koyahness <cryptomakoya@gmail.com>
2026-01-19 18:57:20 +0200

    add inheritance docs

---
+++ in-tutorial/codeimports.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// 
+// OpenZeppelin
+// OpenZeppelin has a robust library of well-documented smart contracts. These include a number of standard-compliant token implementations and a suite of utilities. All the contracts are audited and are therefore safer to use than random code you might find on the internet (you should still do your own audits before releasing to production).
+
+import "./EnumerableSet.sol";
+
