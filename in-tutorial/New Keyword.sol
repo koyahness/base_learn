a1b0fae4e68ee88fe2f1354557d8e9543e3f0c26
koyahness <cryptomakoya@gmail.com>
2026-01-19 19:57:22 +0200

    add inheritance docs

--- in-tutorial/New Keyword.sol
+++ in-tutorial/New Keyword.sol
@@ -6,9 +6,14 @@ pragma solidity ^0.8.0;
 //A contract factory is a contract that creates other contracts
 
 
+// create a contract that creates other contracts.
+//A contract factory is a contract that creates other contracts
+
+
 //Imagine you want to create a contract that can store its owner’s name and complement it upon request. You can create this contract fairly easily.
 
 contract Complimenter {
+    
     string public name;
 
     constructor(string memory _name) {
@@ -19,13 +24,17 @@ contract Complimenter {
         return string.concat("You look great today, ", name);
     }
 }
+
+
+
 //it takes time and effort to manually deploy a new version of the contract for each customer.
 //Luckily, there’s a way to write a contract that will act as a self-service portal for your customers.
 
 contract ComplimenterFactory {
 
-    function CreateComplimenter(string memory _name) public returns (address){
-        new Complimenter(_name)
+    function CreateComplimenter(string memory _name) public returns (address) {
+        Complimenter newContract = new Complimenter(_name);
+        return address(newContract);
     }
 
 }
