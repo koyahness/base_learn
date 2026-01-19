ad64f389e262e187c696cbbf5147b9584e6db966
koyahness <cryptomakoya@gmail.com>
2026-01-19 19:07:34 +0200

    add contract

--- in-tutorial/codeimports.sol
+++ in-tutorial/codeimports.sol
@@ -8,6 +8,7 @@ pragma solidity ^0.8.0;
 
 import "./EnumerableSet.sol";
 
+contract SetExploration {
 //To use the EnumerableSet, you need to use the using keyword. This directive attaches all of the library methods to the type. 
 using EnumerableSet for EnumerableSet.AddressSet;
 
@@ -16,6 +17,8 @@ EnumerableSet.AddressSet private visitors;
 // Add a function called registerVisitor that makes use of the library’s add function to add the sender of the message to the visitors set.
 
 //Add a function called registerVisitor that makes use of the library’s add function to add the sender of the message to the visitors set.
+function registerVisitor() public {
+    visitors.add(msg.sender);
+}
 
-
-
+}
