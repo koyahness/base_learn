79e770f5b373ef70bfe0c29b9d4fa716337cc075
koyahness <cryptomakoya@gmail.com>
2026-01-19 11:13:25 +0200

    ContractA says that it is “contract A” and ContractB says that it is “contract B”.

--- in-tutorial/inheritance.sol
+++ in-tutorial/inheritance.sol
@@ -4,3 +4,18 @@ pragma solidity ^0.8.0;
 
 // Solidity is an object-oriented language. Contracts can inherit from one another, allowing efficient reuse of code.
 // impact inheritance has on the byte code size limit
+
+//ContractA says that it is “contract A” and ContractB says that it is “contract B”.
+
+contract ContractB {
+    function whoAmI() external pure returns (string memory) {
+        return "contract B";
+    }
+}
+
+
+contract ContractA {
+    function whoAmI() external pure returns (string memory) {
+        return "contract A";
+    }
+}
