dd25b7ba745aab4337e5a2945835fcb7ded73791
koyahness <cryptomakoya@gmail.com>
2026-01-19 15:40:19 +0200

    add inheritance docs

--- in-tutorial/inheritance_exercise.sol
+++ in-tutorial/inheritance_exercise.sol
@@ -2,16 +2,41 @@
 
 pragma solidity ^0.8.0;
 
+
+
 abstract contract Employee {
 
 uint public idNumber;
 uint public managerId;
 
 
+
+
+
+
 constructor (uint _idNumber, uint _managerId) {
+
+    idNumber = _idNumber;
+    managerId = _managerId;
+
+}
+
+
+
+
+
+
+function getAnnualCost() public virtual view returns {
+
+}
+
 
 
 }
+
+
+
+contract Salaried is Employee  {
 
 
 
