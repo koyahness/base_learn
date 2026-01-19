bdb8f58adbd221c9d9cee4ddab6238f56a18690b
koyahness <cryptomakoya@gmail.com>
2026-01-19 11:15:35 +0200

    add inheritance docs

---
+++ in-tutorial/inheritance2.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// Solidity is an object-oriented language. Contracts can inherit from one another, allowing efficient reuse of code.
+// impact inheritance has on the byte code size limit
+
+//Contract A says that it is “contract A” and ContractB says that it is “contract B”.
+
+contract ContractB {
+    function whoAmI() external pure returns (string memory) {
+        return "contract B";
+    }
+}
+
+
+contract ContractA {
+    function whoAmI() external pure returns (string memory) {
+        return "contract A";
+    }
+}
