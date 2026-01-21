dcaf10be0fd6a9f040624cdd5b9a05bf99440428
koyahness <cryptomakoya@gmail.com>
2026-01-21 14:18:32 +0200

    add contract to test new

---
+++ in-tutorial/new_exercise.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+//knowledge of the new keyword.
+
+contract AddressBook {
+    function addContact() public {};
+
+    function addContact() public {};
+
+}
+
+
+contract AddressBookFactory{
+
+    AddressBook newContract = new AddressBook();
+    
+}
