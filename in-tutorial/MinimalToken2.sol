1bee8cd5eafde44ad939e47d2f67b1b6b9607e11
koyahness <cryptomakoya@gmail.com>
2026-01-23 19:31:34 +0200

    Redeploy and test again. Now, each of the first three accounts should have 1000 tokens.

--- in-tutorial/MinimalToken2.sol
+++ in-tutorial/MinimalToken2.sol
@@ -18,4 +18,7 @@ contract MinimalToken {
     balances[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = totalSupply / 3;
     balances[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = totalSupply / 3;
 }
+
+// Redeploy and test again. Now, each of the first three accounts should have 1000 tokens.
+
 }
