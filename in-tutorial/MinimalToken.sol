52a1a31a4a7031037190a400c6a0a29c3f5dd6c9
koyahness <cryptomakoya@gmail.com>
2026-01-23 19:27:40 +0200

    add constructor

--- in-tutorial/MinimalToken.sol
+++ in-tutorial/MinimalToken.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//
+// The minimal elements needed for a token are pretty basic.
+// Start by creating a contract called MinimalToken.
+// Add a mapping to relate user addresses to the number of tokens they possess.
+// Finally, add a variable to track totalSupply
+
 contract MinimalToken {
     mapping (address => uint) public balances;
     uint public totalSupply;
+
+    // Add a constructor that initializes the totalSupply at 3000 and assigns ownership to the contract creator
+    constructor() {
+    totalSupply = 3000;
+
+    balances[msg.sender] = totalSupply;
+
+    
+}
 }
