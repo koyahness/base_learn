3de0875754119483b307c7d3299090355b5f4ad1
koyahness <cryptomakoya@gmail.com>
2026-01-24 08:40:28 +0200

    add Unburnable toekn contract

---
+++ in-tutorial/UnburnableToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract UnburnableToken {
+
+mapping (address => uint) public balances;
+uint public totalSupply;
+uint public totalClaimed;
+mapping (address => bool) public claimed;
+
+constructor {
+    totalSupply = 100,000,000;
+}
+
+
+error AllTokensClaimed(uint _totalSupply)
+
+function claim (uint _amountClaimed) public {
+    if (balances[msg.sender] == 0) {
+    if (totalSupply >= _amountClaimed){
+        balances[msg.sender] += _amountClaimed
+        totalClaimed += _amountClaimed
+        totalSupply -= _amountClaimed
+    } else {revert AllTokensClaimed(totalSupply);}
+    } else {revert TokensClaimed();}
+}
+
+
+
+
+
+
+}
