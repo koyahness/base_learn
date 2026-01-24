4b34f54205e17357447c6be8423d50f2973f576c
koyahness <cryptomakoya@gmail.com>
2026-01-24 15:42:44 +0200

    WeightedVoting contract- updated

--- in-tutorial/ERC20exercise.sol
+++ in-tutorial/ERC20exercise.sol
@@ -3,16 +3,19 @@ pragma solidity ^0.8.17;
 
 // libraries
 import "./EnumerableSet.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 using EnumerableSet for EnumerableSet.AddressSet;
 
-contract WeightedVoting {
+contract WeightedVoting is ERC20 {
 
 uint public maxSupply;
+// Issue[] public issues;
 
 constructor {
     maxSupply = 1,000,000;
 }
+
 
 
 error TokensClaimed()
@@ -22,10 +25,7 @@ error QuorumTooHigh() //returning the quorum amount proposed
 error AlreadyVoted()
 error VotingClosed()
 
-
-
 struct Issue {
-
     EnumerableSet.AddressSet private voters;
     string issueDesc;
     uint votesFor;
@@ -35,8 +35,30 @@ struct Issue {
     uint quorum;
     bool passed;
     bool closed;
-    
 }
+
+enum Vote { AGAINST, FOR, ABSTAIN }
+
+Issue[] public issues;
+
+//mint to other users. 
+constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+    _mint(msg.sender, maxSupply);
+}
+
+
+
+
+function claim () public {
+uint claimTokens = 100;
+
+if (maxSupply >=  claimTokens){
+
+}
+
+}
+
+
 
 
 
